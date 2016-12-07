ca06951f0eb2b1671690c18572d926834fd57f71
Thomas Bollaert <thomas.bollaert@gmail.com>
2016-12-07 14:14:37 +0100

    Update Assignment 6.5.py

--- week6/Assignment 6.5.py
+++ week6/Assignment 6.5.py
@@ -1,6 +1,13 @@
+
+#Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. 
+#Convert the extracted value to a floating point number and print it out.
+
 str = "X-DSPAM-Confidence:  0.8475"
 location1 = str.find(" ")
 location2 = str.find("5")
 number = str[location1:location2+1]
 num = float(number.strip())
 print num
+
+#desired output:
+# 0.8475
