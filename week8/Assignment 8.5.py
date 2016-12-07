b4965821e822be4fc58ef41a1e1786c99550c9f9
Thomas Bollaert <thomas.bollaert@gmail.com>
2016-12-07 14:31:01 +0100

    Update Assignment 8.5.py

--- week8/Assignment 8.5.py
+++ week8/Assignment 8.5.py
@@ -1,6 +1,7 @@
 #Open the file mbox-short.txt and read it line by line. When you find a line that starts with 'From ' like the following line:
 
 #From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+
 #You will parse the From line using split() and print out the second word in the line 
 #(i.e. the entire address of the person who sent the message). Then print out a count at the end.
 #Hint: make sure not to include the lines that start with 'From:'.
