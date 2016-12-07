e97a7ecd118834173f78c76f1cf8201ac976dd11
Thomas Bollaert <thomas.bollaert@gmail.com>
2016-12-07 16:49:54 +0100

    Update Assignment 10.2.py

--- Week10/Assignment 10.2.py
+++ Week10/Assignment 10.2.py
@@ -1,4 +1,4 @@
-Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for 
+#Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for 
 #each of the messages. You can pull the hour out from the 'From ' line by finding the time and then splitting 
 #the string a second time using a colon.
 
