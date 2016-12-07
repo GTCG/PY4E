65b38e35722bde55a5b1deb8613e0e6017979d92
Thomas Bollaert <thomas.bollaert@gmail.com>
2016-12-07 14:22:34 +0100

    Update Assignment 7.2.py

--- week7/Assignment 7.2.py
+++ week7/Assignment 7.2.py
@@ -1,3 +1,13 @@
+#Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+
+#X-DSPAM-Confidence:    0.8475
+
+#Count these lines and extract the floating point values from each of the lines and compute the average of those values 
+#and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
+
+#You can download the sample data at http://www.pythonlearn.com/code/mbox-short.txt when you are testing below 
+#enter mbox-short.txt as the file name.
+
 # Use the file name mbox-short.txt as the file name
 countx = 0
 countline = 0
@@ -15,6 +25,8 @@ for line in fhand:
 		fhand.close
 print "Average spam confidence:", countx / countline
 
+#desired output:
+#Average spam confidence: 0.750718518519
 
 		
 		
