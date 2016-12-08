398b3c9103db16d00005838dc0ef352a22c48eb7
Thomas Bollaert <thomas.bollaert@gmail.com>
2016-12-08 10:12:13 +0100

    Update Degrees Celsius-Fahrenheit.py

--- Other-assignments/Degrees Celsius-Fahrenheit.py
+++ Other-assignments/Degrees Celsius-Fahrenheit.py
@@ -2,6 +2,6 @@ input =input("How many degrees Celsius?")
 try:    
   C = float(input)
 except:
-  print ("please enter a valid number")
+  print ("please enter a valid number! No "," allowed")
 F =  C * 1.8 + 32
 print (C, "degrees Celsius is equal to", F, "degrees Fahrenheit")
