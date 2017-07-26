53cc97c01aea2d323def50109fe2ce4e1ec2d89f
Thomas Bollaert <thomas.bollaert@gmail.com>
2017-07-26 19:54:43 +0200

    Create Assignment 3.1.py

---
+++ python3.x/Assignment 3.1.py
@@ -0,0 +1,14 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+rph = input("enter rate per hour:")
+r = float(rph)
+if h<=40:
+	result = h*r
+else:
+    o = h-40
+    result = o*(r*1.50) + 40*r
+
+print (result)
+
+#desired output
+#498.75
