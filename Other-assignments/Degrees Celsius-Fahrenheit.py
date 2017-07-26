40aff8eb3176975293e714c7f365f318afe0a9fd
Thomas Bollaert <thomas.bollaert@gmail.com>
2017-07-26 19:13:41 +0200

    Delete Degrees Celsius-Fahrenheit.py

--- Other-assignments/Degrees Celsius-Fahrenheit.py
+++
@@ -1,7 +0,0 @@
-input =input("How many degrees Celsius?")
-try:    
-  C = float(input)
-except:
-  print ("please enter a valid number! No "," allowed")
-F =  C * 1.8 + 32
-print (C, "degrees Celsius is equal to", F, "degrees Fahrenheit")
