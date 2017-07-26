b33362099651e6a4b8e3d9b0ee0fd0aca20c216c
Thomas Bollaert <thomas.bollaert@gmail.com>
2017-07-26 20:02:40 +0200

    Create Assignment 5.2.py

---
+++ python3.x/Assignment 5.2.py
@@ -0,0 +1,29 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+
+    #input
+    if num == "done" : break
+    if len(num) == 0 : break
+
+    try:
+    	num = int(num)
+    	if smallest is None or smallest > num :
+    		smallest = num;
+    		
+    	if largest is None or largest < num :
+    		largest = num;
+
+    except Exception, e:
+    	print ("Invalid input")
+
+print ("Maximum is", largest)
+print ("Minimum is", smallest)
+
+#desired output
+#Invalid input
+#Maximum is 7
+#Minimum is 4
+
+#Watch the Capitals!!
