9005d053414e5a78e5775e08d40e082088232d68
Thomas Bollaert <thomas.bollaert@gmail.com>
2017-07-26 19:57:16 +0200

    Create Assignment 3.3.py

---
+++ python3.x/Assignment 3.3.py
@@ -0,0 +1,17 @@
+score = input("Enter Score: ")
+digit =float(score)
+if digit >1 or digit <0:
+    print ("out of range")
+elif digit >=0.9:
+    print ("A")
+elif digit >=0.8:
+    print ("B")
+elif digit >=0.7:
+    print ("C")
+elif digit >= 0.6:
+    print ("D")
+else:
+    print ("F")
+
+#desired output:
+#B
