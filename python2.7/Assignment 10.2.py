6ef543f168c497963723e797198a40e615b4fce5
UDIT BHASKAR <59888244+uditbhaskar21@users.noreply.github.com>
2020-05-16 00:51:31 +0530

    Update Assignment 10.2.py

--- python2.7/Assignment 10.2.py
+++ python2.7/Assignment 10.2.py
@@ -6,34 +6,20 @@
 
 #Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
 
-name = raw_input("Enter file:")
+name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
-t = list()
-d = dict()
+ti = list()
+di = dict()
 for line in handle:
     if line.startswith("From "):
 		line = line.rstrip()
 		words = line.split()
 		time = words[5]
 		hour = time[0:2]
-		d[hour] = d.get(hour,0) +1
-#for key, val in d.items():
-t = d.items()
-t.sort()
-for key, val in t:
-    print key, val
-    
-#desired output:
-#04 3
-#06 1
-#07 1
-#09 2
-#10 3
-#11 6
-#14 1
-#15 2
-#16 4
-#17 2
-#18 1
-#19 1
+		di[hour] = di.get(hour,0) +1
+
+ti = di.items()
+ti.sort()
+for key, val in ti:
+    print(key, val)
