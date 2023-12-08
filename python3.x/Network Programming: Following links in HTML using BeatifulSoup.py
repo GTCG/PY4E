5b4f89b0aca06e14edff365f1855eeef62a72282
Thomas Bollaert <thomas.bollaert@gmail.com>
2023-12-08 18:21:09 +0100

    Update Network Programming: Following links in HTML using BeatifulSoup.py

--- python3.x/Network Programming: Following links in HTML using BeatifulSoup.py
+++ python3.x/Network Programming: Following links in HTML using BeatifulSoup.py
@@ -5,30 +5,28 @@
 #extract the href= vaues from the anchor tags, scan for a tag that is in a particular position relative to the 
 #first name in the list, follow that link and repeat the process a number of times and report the last name you find. 
 
-#This is a sample program and I do not guarantee this program is the solution to the actual assignment, as it differs
-#from time to time.
+#This is a sample program Please paste the URL given in the assignment and follow the instructions when entering a number for count and position.
 
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-import urllib
-from BeautifulSoup import *
-
-count = int(input("enter count: "))
-position= int(input("enter position: "))
+count = int(input("Enter count: "))
+position = int(input("Enter position: "))
 number = 0
-number2 = 0
 
-url = "http://python-data.dr-chuck.net/known_by_Calah.html"
-print (url)
-html = urllib.urlopen(url).read()
+url = "http://py4e-data.dr-chuck.net/known_by_Dilraj.html"
+print(url)
 
-while number<count:
-	number = 0
-	soup = BeautifulSoup(html)
-	tags = soup("a")
-	for tag in tags:
-		number = number + 1
-		while number2 < position:
-			url = tag.get("href", None)
-			print (url)
-			html = urllib.urlopen(url).read()
-			number2 +=1
+while number < count:
+    number += 1
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
+    tags = soup("a")
+    
+    number2 = 0
+    for tag in tags:
+        number2 += 1
+        if number2 == position:
+            url = tag.get("href", None)
+            print(url)
+            break
