286e71e902eb18102b8626d712a71bfaaf427e1f
Thomas Bollaert <thomas.bollaert@gmail.com>
2023-12-10 16:16:36 +0100

    Update Assignment 10.2.py

--- python3.x/Assignment 10.2.py
+++ python3.x/Assignment 10.2.py
@@ -1,17 +1,19 @@
 name = input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
+if len(name) < 1: name = "mbox-short.txt"
 handle = open(name)
-t = list()
 d = dict()
+
 for line in handle:
     if line.startswith("From "):
-		line = line.rstrip()
-		words = line.split()
-		time = words[5]
-		hour = time[0:2]
-		d[hour] = d.get(hour,0) +1
-#for key, val in d.items():
-t = d.items()
-t.sort()
-for key, val in t:
-    print (key, val)
+        line = line.rstrip()
+        words = line.split()
+        time = words[5]
+        hour = time.split(":")[0]
+        d[hour] = d.get(hour, 0) + 1
+
+# Sort the dictionary by keys
+sorted_d = dict(sorted(d.items()))
+
+# Print the sorted dictionary
+for key, val in sorted_d.items():
+    print(key, val)
