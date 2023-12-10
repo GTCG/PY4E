d23e7ec0be688cd3c8ff999a33256419c6eae925
Thomas Bollaert <thomas.bollaert@gmail.com>
2023-12-10 16:20:46 +0100

    Update Network programming: Understanding the request-response cycle.py

--- python3.x/Network programming: Understanding the request-response cycle.py
+++ python3.x/Network programming: Understanding the request-response cycle.py
@@ -1,3 +1,28 @@
+'''
+Exploring the HyperText Transport Protocol
+
+You are to retrieve the following document using the HTTP protocol in a way that you can examine the HTTP Response headers.
+
+http://data.pr4e.org/intro-short.txt
+There are three ways that you might retrieve this web page and look at the response headers:
+
+Preferred: Modify the socket1.py program to retrieve the above URL and print out the headers and data. Make sure to change the code to retrieve the above URL - the values are different for each URL.
+Open the URL in a web browser with a developer console or FireBug and manually examine the headers that are returned.
+Enter the header values in each of the fields below and press "Submit".
+
+Last-Modified:
+
+ETag:
+
+Content-Length:
+
+Cache-Control:
+
+Content-Type:
+
+
+'''
+
 #assignment belongs to chapter 13: Network Programming
 import socket
 
