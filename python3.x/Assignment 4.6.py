286a34ef34c251fd2b97fe898bc0f4fba7076444
Thomas Bollaert <thomas.bollaert@gmail.com>
2023-12-13 10:48:51 +0100

    Update Assignment 4.6.py

--- python3.x/Assignment 4.6.py
+++ python3.x/Assignment 4.6.py
@@ -1,13 +1,13 @@
-def computepay(h,r):
-	if h<=40:
-		result = h*r
-	else:
-   	    o = h-40    	
-	result = o*(r*1.50) + 40*r
-	return result
-
-hours = input("enter hours:")
-rate = input("enter rate:")
+def computepay(hours, rate):
+    if hours<=40:
+        result = hours * rate
+        return result
+    else:
+        overtime = hours - 40
+        result = overtime*(rate*1.50) + 40*rate
+        return result
+hours = input("enter hours: ")
+rate = input ("enter rate: ")
 
 try:
     hours = float(hours)
@@ -15,8 +15,8 @@ try:
 except:
     print ("please enter a valid value.")
     quit()
-p = computepay(hours, rate)
-print ("Pay",(str(p)))
+pay = computepay(hours, rate)
+print ("Pay", (str(pay)))
 
 #desired output
 #498.75
