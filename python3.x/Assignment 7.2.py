59fb26d1c2244018c154ad1a8c7acfdb0fefdcb3
Thomas Bollaert <thomas.bollaert@gmail.com>
2023-12-13 11:53:24 +0100

    Update Assignment 7.2.py

--- python3.x/Assignment 7.2.py
+++ python3.x/Assignment 7.2.py
@@ -1,19 +1,21 @@
 countx = 0
 countline = 0
-fname = input("open file name: ")
+fname= input("enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
 try:
     fhand = open(fname)
 except:
-    print ("file not found, ", fname)
+    print ("file not found, " , fname)
+    quit()
 for line in fhand:
     line = line.rstrip()
-    if line.startswith ("X-DSPAM-Confiden"):
-        #print (line)
+    if line.startswith("X-DSPAM-Confidence: "):
         stripped = line.strip("X-DSPAM-Confidence: ")
+        #print (stripped)
         countx = countx + float(stripped)
-        countline = countline + 1
-        fhand.close
-print ("Average spam confidence:", countx / countline)
+        countline = countline +1
+fhand.close()
+print ("Average spam confidence:" , countx/ countline)
 
 #desired output:
 #Average spam confidence: 0.750718518519
